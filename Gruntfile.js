269a10ca7f805931fedeb7559ce79f355091680a
Rich Leland <rich@richleland.com>
2013-10-03 18:08:08 -0400

    Set up grunt tasks for building sinon

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var basePath = path.resolve('.');
+
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    clean: ['sinon*', 'build/'],
+
+    shell: {
+      gitclone: {
+        command: 'git clone git@github.com:cjohansen/Sinon.JS.git build/Sinon.JS'
+      },
+      gitcheckout: {
+        command: 'cd build/Sinon.JS/ && git checkout <%= grunt.option("version") %>'
+      },
+      buildsinon: {
+        command: 'cd build/Sinon.JS/ && ./build'
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['build/Sinon.JS/pkg/*'], dest: '.', flatten: true, filter: 'isFile'}
+        ]
+      }
+    }
+
+  });
+
+  grunt.registerTask('build', 'Builds a release of Sinon.js', function(version) {
+    version = version || "master";
+    if(version && version !== 'master' && version.indexOf('v') !== 0) {
+      version = 'v' + version;
+    }
+    grunt.option('version', version);
+    grunt.task.run([
+      'clean',
+      'shell:gitclone',
+      'shell:gitcheckout',
+      'shell:buildsinon',
+      'copy'
+    ]);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-shell');
+
+  // default task
+  grunt.registerTask('default', ['build']);
+};
--- README.md
+++ README.md
@@ -2,3 +2,21 @@ bower-sinon
 ===========
 
 Bower repo for Sinon.js
+
+## Creating a release of Sinon.js
+
+```
+# clone this repo
+git clone git@github.com:messagesystems/bower-sinon
+cd bower-sinon
+
+# install the juicer gem (required by sinon to build)
+gem install juicer
+
+# install grunt
+npm install -g grunt-cli
+npm install
+
+# run the build via grunt
+grunt build[:version]
+```
